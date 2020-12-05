44528998bde6068f02feffcbe9cca3dfe5a0c1fa
Forbes Lindesay <forbes@lindesay.co.uk>
2020-12-05 04:34:07 +0000

    feat: favicon and loading spinner example

--- packages/example/scripts/index.tsx
+++ packages/example/scripts/index.tsx
@@ -13,7 +13,7 @@ export default function App() {
   const {value} = useObservableState(observable);
   return (
     <>
-      <div className="pt-10 pb-12 px-4 sm:px-6 lg:px-8 bg-gray-200 dark:bg-gray-900 sm:pb-16">
+      <div className="flex-shrink-0 pt-10 pb-12 px-4 sm:px-6 lg:px-8 bg-gray-200 dark:bg-gray-900 sm:pb-16">
         <dl className="max-w-xs mx-auto rounded-lg bg-white dark:bg-gray-800 shadow-lg flex flex-col border border-gray-200 dark:border-gray-700 p-6 text-center">
           <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500 dark:text-gray-300">
             Server Count
@@ -22,6 +22,31 @@ export default function App() {
             {value}
           </dd>
         </dl>
+      </div>
+      <div className="flex flex-shrink-0 py-4 px-6">
+        <div className="flex items-center rounded-full text-gray-800 dark:text-gray-300 bg-gray-300 dark:bg-gray-800 h-12 px-4">
+          <svg
+            className="animate-spin h-5 w-5 text-black dark:text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          <div className="ml-3">TypeScript checks in progress</div>
+        </div>
       </div>
       <pre className="flex-grow px-2 sm:px-8 pt-10 pb-16 bg-gray-100 dark:bg-gray-700 text-indigo-900 dark:text-indigo-100">
         <code>{pkg}</code>
